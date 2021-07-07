fb0d82ed990bd07f58bd714be867fc69894c1d94
BubbleTrouble14 <ronald.goedeke@outlook.com>
2021-07-07 16:46:32 +0200

    update to app

--- app.js
+++ app.js
@@ -1,49 +1,16 @@
 const http = require("http");
 var schedule = require("node-schedule");
 const fetch = require("node-fetch");
-let data;
+let data = "{Test}";
 const port = process.env.PORT || 3000;
 
-const server = http.createServer(function (req, res) {
-  if (req.method === "POST") {
-    var body = "";
+const server = http.createServer((req, res) => {
+  res.setHeader("Content-type", "application/json");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.writeHead(200); //Status code OK
 
-    req.on("data", function (chunk) {
-      body += chunk;
-    });
-
-    req.on("end", function () {
-      if (req.url === "/") {
-        log("Received message: " + body);
-      } else if ((req.url = "/scheduled")) {
-        log(
-          "Received task " +
-            req.headers["x-aws-sqsd-taskname"] +
-            " scheduled at " +
-            req.headers["x-aws-sqsd-scheduled-at"]
-        );
-      }
-
-      res.writeHead(200, "OK", { "Content-Type": "text/plain" });
-      res.end();
-    });
-  } else {
-    res.setHeader("Content-type", "application/json");
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.writeHead(200);
-    res.end(JSON.stringify(data));
-    // res.write(html);
-    // res.end();
-  }
+  res.end(JSON.stringify(data));
 });
-
-// const server = http.createServer((req, res) => {
-//   res.setHeader("Content-type", "application/json");
-//   res.setHeader("Access-Control-Allow-Origin", "*");
-//   res.writeHead(200); //Status code OK
-
-//   res.end(JSON.stringify(data));
-// });
 
 const fetchCurrencyExchange = () => {
   return fetch(
@@ -66,8 +33,13 @@ schedule.scheduleJob("0 */6 * * *", function () {
   });
 });
 
-server.listen(port);
-
-fetchCurrencyExchange();
+server.listen(port, () => {
+  fetchCurrencyExchange().then((response) => {
+    if (response) {
+      console.log(response);
+      data = response;
+    }
+  });
+});
 
 console.log("Server running at http://127.0.0.1:" + port + "/");
